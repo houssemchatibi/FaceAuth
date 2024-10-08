3589b7f645e4c1827c7ab6fbfa7859d1c383698e
houssemchatibi <houssem.chatibi@gmail.com>
2024-10-08 01:52:56 +0100

    delete config.py

--- backend/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:houssem123@localhost/faceRecognition')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
