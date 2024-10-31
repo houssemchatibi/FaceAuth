4f972ac93a9e638b11aa3d71e019485929650d01
houssemchatibi <houssem.chatibi@gmail.com>
2024-10-31 02:30:17 +0100

    add UI Signup & handle errors

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Webcam from 'react-webcam';
 import { fetchUserConnect, logout } from '../redux/userSlice';
+import { Link } from 'react-router-dom';
 //import Signup from './Signup'
 
 const Login = () => {
@@ -17,50 +18,50 @@ const Login = () => {
     const errorMessage = useSelector((state) => state.user.error);
 
     const handleSubmit = async () => {
-        
-            // Capture de l'image depuis la webcam
-            const imageSrc = await webcamRef.current.getScreenshot();
-            if (!imageSrc) {
-                throw new Error("Unable to capture image from webcam");
-            }
 
-            // Utilise une variable temporaire pour stocker l'image capturée
-            const updatedInputs = {
-                ...inputs,
-                faceImage: imageSrc,
-            };
+        // Capture de l'image depuis la webcam
+        const imageSrc = await webcamRef.current.getScreenshot();
+        if (!imageSrc) {
+            throw new Error("Unable to capture image from webcam");
+        }
 
-            if (!updatedInputs.faceImage) {
-                throw new Error("Missing face image");
-            }
+        // Utilise une variable temporaire pour stocker l'image capturée
+        const updatedInputs = {
+            ...inputs,
+            faceImage: imageSrc,
+        };
 
-            dispatch(fetchUserConnect(updatedInputs));
+        if (!updatedInputs.faceImage) {
+            throw new Error("Missing face image");
+        }
+
+        dispatch(fetchUserConnect(updatedInputs));
 
 
-            // Reset fields if login is successful
-            // setInputs({ username: "", faceImage: null });
+        // Reset fields if login is successful
+        // setInputs({ username: "", faceImage: null });
 
-            console.log("well connecte")
-            console.log("the connected user :",inputs)
+        console.log("well connecte")
+        console.log("the connected user :", inputs)
 
     }
 
 
     return (
         <>
-            <div className="h-screen bg-gray-100 mx-auto  py-12 px-8 justify-center items-center">
-                <div className="mx-auto max-w-md px-6 py-6 bg-white border-0 shadow-lg rounded-3xl">
+            <div className="min-h-screen bg-gray-100 mx-auto  py-10 px-8 justify-center items-center">
+                <div className="mx-auto max-w-md px-6 py-4 bg-white border-0 shadow-lg rounded-3xl">
                     <center>
-                        <h1 className="text-2xl font-bold mb-8">Face-Auth</h1>
+                        <h1 className="text-2xl font-bold mb-4">Face-Auth</h1>
                     </center>
-                    <label className="text-l mb-8">username : </label>
+                    <label className="text-l mb-4">username : </label>
                     <input
                         type="text"
                         name="username"
                         placeholder=" "
                         required
                         onChange={(e) => { setInputs({ ...inputs, username: e.target.value }) }}
-                        className="w-full rounded-md py-2.5 px-4 border text-sm outline-[#777575] my-5"
+                        className="w-full rounded-md py-2.5 px-4 border text-sm outline-[#777575] mt-2 "
                     />
 
 
@@ -69,14 +70,20 @@ const Login = () => {
                     )}
 
 
-                    <div>
+                    <div className="rounded-lg overflow-hidden mt-4" >
                         <Webcam ref={webcamRef} screenshotFormat="image/jpeg" />
                     </div>
-                    <div className='flex justify-around'>
-                        <button className="mt-4 px-4 py-2 bg-blue-500 text-white text-lg font-medium rounded-md" onClick={handleSubmit}>Log in</button>
-                        <button className="mt-4 px-4 py-2 bg-blue-500 text-white text-lg font-medium rounded-md" onClick={() => dispatch(logout())}>Sign up</button>
+                    <div className="flex flex-col items-center">
+                        <button className="mt-4 px-4 py-2 bg-blue-500 text-white text-lg font-medium rounded-md w-1/3" onClick={handleSubmit}>Log in</button>
+                        <p className='text-sm font-light text-gray-500 mt-2'>
+                            {"Don't"} have an account?{" "}
+                            <Link to='/signup' className='font-medium text-primary-600 hover:underline text-blue-600'>
+                                Sign Up
+                            </Link>
+                        </p>
                     </div>
                 </div>
+               
             </div>
         </>
     )
